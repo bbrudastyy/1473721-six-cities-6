32b5870b6a8dbe99ead82596c5abc79f78dc0224
Богдан Котов <bbrudastyy@gmail.com>
2021-02-04 23:15:17 +0300

    Добавлена маршрутизация

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import MainScreen from '../main-screen/main-screen';
+import LoginScreen from '../login-screen/login';
+import FavoritesScreen from '../favorites-screen/favorites';
+import OfferScreen from '../offer-screen/offer-screen';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const App = ({offersCount}) => {
   return (
-    <MainScreen
-      offersCount={offersCount}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainScreen offersCount={offersCount} />
+        </Route>
+        <Route exact path="/login">
+          <LoginScreen />
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritesScreen />
+        </Route>
+        <Route exact path="/offer/:id">
+          <OfferScreen />
+        </Route>
+        <Route>
+          <NotFoundScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
