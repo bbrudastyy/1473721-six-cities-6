43aae471273eef27d5ad6604c583bf1f19f27dec
Богдан Котов <bbrudastyy@gmail.com>
2021-02-04 22:36:23 +0300

    Добавлены необходимые компоненты и исправлены предыдущие замечания

--- src/components/main-screen/main-screen.jsx
+++ src/components/main-screen/main-screen.jsx
@@ -1,33 +1,13 @@
 import React from 'react';
-import Card from '../card';
+import Card from '../card/card';
+import Header from '../header/header';
 import PropTypes from 'prop-types';
 
 const cardsOffers = [1, 2, 3, 4, 5];
 
 const MainScreen = ({offersCount}) => {
   return <div className="page page--gray page--main">
-    <header className="header">
-      <div className="container">
-        <div className="header__wrapper">
-          <div className="header__left">
-            <a className="header__logo-link header__logo-link--active">
-              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width={81} height={41} />
-            </a>
-          </div>
-          <nav className="header__nav">
-            <ul className="header__nav-list">
-              <li className="header__nav-item user">
-                <a className="header__nav-link header__nav-link--profile" href="#">
-                  <div className="header__avatar-wrapper user__avatar-wrapper">
-                  </div>
-                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </header>
+    <Header/>
     <main className="page__main page__main--index">
       <h1 className="visually-hidden">Cities</h1>
       <div className="tabs">
@@ -87,7 +67,7 @@ const MainScreen = ({offersCount}) => {
               </ul>
             </form>
             <div className="cities__places-list places__list tabs__content">
-              {cardsOffers.map((card, i) => <Card key={card + i} />)}
+              {cardsOffers.map((card, i) => <Card key={`card_${i}`} />)}
             </div>
           </section>
           <div className="cities__right-section">
